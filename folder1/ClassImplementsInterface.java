dab1ffa10ece67fb186630ecb59d959e71566954
Divya Kundale <74963709+DivyaKundale-create@users.noreply.github.com>
2026-07-30 16:24:46 +0530

    Create ClassImplementsInterface.java

---
+++ folder1/ClassImplementsInterface.java
@@ -0,0 +1,8 @@
+package org.example.classes;
+
+public class ClassImplementsInterface implements NewInterface{
+    @Override
+    public void display() {
+        System.out.println("This is public abstract method by default");
+    }
+}
